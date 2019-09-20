4427e5c1ec6ee24c412f1d503f437a4ae60b3646
Luiz Filipe <moresco.luiz@gmail.com>
2019-09-19 22:07:48 -0300

    Improve flask config

--- tmdb/flask.py
+++ tmdb/flask.py
@@ -3,7 +3,7 @@ from flask import Flask
 
 from tmdb.views import api
 
-app = Flask(__name__, instance_relative_config=True)
+app = Flask(__name__, instance_relative_config=False)
 
 app.config.from_pyfile('config.py', silent=True)
 app.register_blueprint(api, url_prefix='/api/v1')
