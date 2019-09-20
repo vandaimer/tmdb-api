780b64b7bf0b83329219dcc29b17ccd16e2ef85a
Luiz Filipe <moresco.luiz@gmail.com>
2019-09-19 22:28:43 -0300

    Implement get_image_url method on TMDBService class

--- tmdb/services/tmdb.py
+++ tmdb/services/tmdb.py
@@ -6,6 +6,7 @@ from tmdb import config
 class TMDBService:
 
     MOVIE_BASE_URL = f'{config.TMDB_API_BASE_URL}/movie'
+    IMAGE_BASE_URL = f'{config.TMDB_IMAGE_BASE_URL}/w500'
     BASE_QUERY_PARAMS = f'api_key={config.TMDB_API_KEY}&language=en-US'
 
     @staticmethod
@@ -14,3 +15,7 @@ class TMDBService:
         params = f'{TMDBService.BASE_QUERY_PARAMS}&page=1'
 
         return requests.get(url, params=params).json()
+
+    @staticmethod
+    def get_image_url(image_hash):
+        return f'{TMDBService.IMAGE_BASE_URL}/{image_hash}'
