197b3924db81c82a43b76cf770c56a00016e03ec
Luiz Filipe <moresco.luiz@gmail.com>
2019-09-19 22:29:54 -0300

    Improve the build_movie method to get the image url from service

--- tmdb/views/movie.py
+++ tmdb/views/movie.py
@@ -6,15 +6,19 @@ class Movie:
     def get_upcoming_list():
         movies = TMDBService.get_upcoming_movie_list()
         movies = [Movie.build_movie(movie) for movie in movies['results']]
+
         return {
-            'items': movies,
+                'items': movies,
         }
 
     @staticmethod
     def build_movie(movie):
+        poster = movie.get('poster_path')
+        backdrop = movie.get('backdrop_path')
+
         return {
             'title': movie.get('title'),
             'release_date': movie.get('release_date'),
-            'poster': movie.get('poster_path'),
-            'backdrop': movie.get('backdrop_path'),
+            'poster': TMDBService.get_image_url(poster),
+            'backdrop': TMDBService.get_image_url(backdrop),
         }
