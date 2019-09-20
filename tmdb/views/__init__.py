16cdc5f00f82ba616aa7d43cba27862b288473e9
Luiz Filipe <moresco.luiz@gmail.com>
2019-09-19 22:09:28 -0300

    Implement the /movies route

--- tmdb/views/__init__.py
+++ tmdb/views/__init__.py
@@ -1,6 +1,7 @@
-from flask import Blueprint
+from flask import Blueprint, request, jsonify
 
 from .health_check import HealthCheck
+from .movie import Movie
 
 
 api = Blueprint('api', __name__)
@@ -9,3 +10,8 @@ api = Blueprint('api', __name__)
 @api.route('/healthcheck', methods=['GET'])
 def call_healt_check_view():
     return HealthCheck.get()
+
+
+@api.route('/movies', methods=['GET'])
+def call_movies_upcoming_list():
+    return jsonify(Movie.get_upcoming_list())
