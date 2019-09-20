58d938fee0caac312d385ad257d012ec4bdc4886
Luiz Filipe <moresco.luiz@gmail.com>
2019-09-20 19:26:15 -0300

    Implement get_genre_list method on TMDBService

--- tmdb/services/tmdb.py
+++ tmdb/services/tmdb.py
@@ -6,6 +6,7 @@ from tmdb import config
 class TMDBService:
 
     MOVIE_BASE_URL = f'{config.TMDB_API_BASE_URL}/movie'
+    GENRE_BASE_URL = f'{config.TMDB_API_BASE_URL}/genre/movie/list'
     IMAGE_BASE_URL = f'{config.TMDB_IMAGE_BASE_URL}/w500'
     BASE_QUERY_PARAMS = f'api_key={config.TMDB_API_KEY}&language=en-US'
 
@@ -13,6 +14,13 @@ class TMDBService:
     def get_upcoming_movie_list(page):
         url = f'{TMDBService.MOVIE_BASE_URL}/upcoming'
         params = f'{TMDBService.BASE_QUERY_PARAMS}&page={page}'
+
+        return requests.get(url, params=params).json()
+
+    @staticmethod
+    def get_genre_list():
+        url = TMDBService.GENRE_BASE_URL
+        params = TMDBService.BASE_QUERY_PARAMS
 
         return requests.get(url, params=params).json()
 
