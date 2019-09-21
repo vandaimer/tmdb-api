58db3155cb4eef6f51fe7a7bcf44b6d67cec2b86
Luiz Filipe <moresco.luiz@gmail.com>
2019-09-21 16:24:15 -0300

    Implement a catch_all func to handle unimplemented endpoints

--- tmdb/views/__init__.py
+++ tmdb/views/__init__.py
@@ -1,11 +1,22 @@
 from flask import Blueprint, request, jsonify
+import time
 
 from .health_check import HealthCheck
 from .movie import Movie
 
 
 api = Blueprint('api', __name__)
+catch_all = Blueprint('catch_all', 'catch_all')
 
+
+@catch_all.route('/', defaults={'path': ''})
+@catch_all.route('/<path:path>')
+def catch_all_view(path):
+    return {
+        'path': path,
+        'status': 'notImplemented',
+        'now':  time.time(),
+    }
 
 @api.route('/healthcheck', methods=['GET'])
 def call_healt_check_view():
